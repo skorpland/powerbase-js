7cda09141bee0bbee8c5c6721ebedad4f60169aa
root <root@DESKTOP-QGIQ5RP.localdomain>
2025-04-06 15:27:58 +0530

    Initial commit

--- -/powerbase-js-2.49.3/package/src/lib/SupabaseAuthClient.ts
+++
@@ -1,8 +0,0 @@
-import { AuthClient } from '@skorpland/auth-js'
-import { PowerbaseAuthClientOptions } from './types'
-
-export class PowerbaseAuthClient extends AuthClient {
-  constructor(options: PowerbaseAuthClientOptions) {
-    super(options)
-  }
-}
